5670569703ab10a2e9c3772cfd77ab6d70b0ff05
Alan-AC7 <xxprolegendzxx@gmail.com>
2023-07-16 21:16:23 -0600

    Subiendo proyecto

---
+++ 018_WEB_COLEGIO_MVC/ProyectoWeb/ProyectoWeb/Filters/VerificarSesion.cs
@@ -0,0 +1,42 @@
+﻿using ProyectoWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoWeb.Filters
+{
+    public class VerificarSesion : ActionFilterAttribute
+    {
+        private int? IdUsuario;
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try {
+
+                base.OnActionExecuting(filterContext);
+
+                if (HttpContext.Current.Session["IdUsuario"] != null)
+                    IdUsuario = (int)HttpContext.Current.Session["IdUsuario"];
+                else
+                    IdUsuario = null;
+
+                if (IdUsuario == null) {
+                    if (filterContext.Controller is LoginController == false) {
+                        filterContext.HttpContext.Response.Redirect("/Login/Index");
+                    }
+                }
+
+            }
+            catch (Exception ex) {
+
+                filterContext.Result = new RedirectResult("/Login/Index");
+            }
+
+
+        }
+
+
+    }
+}
